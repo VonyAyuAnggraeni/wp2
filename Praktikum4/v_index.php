18243679604f9a7d95f2b1621a11e97042bd4f13
VonyAyuAnggraeni <57248883+VonyAyuAnggraeni@users.noreply.github.com>
2020-11-01 14:59:40 +0700

    Add files via upload

---
+++ Praktikum4/v_index.php
@@ -0,0 +1,26 @@
+        <section>
+            <h1><?php echo $judul ?></h1>
+            <p align='justify'\'>Pada pengertian codeigniter di atas tadi di jelaskan bahwa codeigniter 
+            menggunakan metode MVC. Apa itu MVC? Kita juga harus mengetahui apa itu MVC sebelum masuk dan 
+            lebih jauh dalam belajar codeigniter.</p>
+            <p>MVC adalah teknik atau konsep yang memisahkan komponen utama menjadi tiga komponen yaitu model, 
+            view dan controller.</p>
+
+            <li>Model</li>
+            <p align='justify'>Model adalah kelas yang merepresentasikan atau memodelkan tipe data yang akan 
+            digunakan oleh aplikasi.</p>
+            <p align='justify'>Sebagai catatan, Semua model harus disimpan di dalam folder application\models       
+            </p> 
+
+            <li>View</li>
+            <p align='justify'>View merupakan bagian yang menangani halaman user interface atau halaman yang 
+            muncul pada user(pada browser).</p> 
+            
+            <li>Controller</li>
+            <p align='justify'>Controller merupakan kumpulan intruksi aksi yang menghubungkan model dan view, 
+            jadi user tidak akan berhubungan dengan model secara langsung, intinya data yang tersimpan di 
+            database (model) di ambil oleh controller dan kemudian controller pula yang menampilkan nya ke 
+            view. Jadi controller lah yang mengolah intruksi.</p>
+
+        </section>
+
